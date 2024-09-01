32e41c427f6795f689558e8197d0205b05861c95
IXtimes <xandercorcoran@gmail.com>
2024-09-01 18:08:48 -0500

    further fixes on .gitignore

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace IXTweaks
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "IXtimes.IXTweaks";
-        public const string PLUGIN_NAME = "IXTweaks";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
