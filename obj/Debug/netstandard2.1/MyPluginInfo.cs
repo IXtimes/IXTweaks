7a644f4846dadb0304685d1da2bed84f71b94cd6
IXtimes <xandercorcoran@gmail.com>
2024-09-01 18:06:28 -0500

    \Inital Commit

---
+++ obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace IXTweaks
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "IXtimes.IXTweaks";
+        public const string PLUGIN_NAME = "IXTweaks";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
